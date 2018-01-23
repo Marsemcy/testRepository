26716e46e34ed0947b473c975ad2168772da8439
Marcela Friptu <friptu.marcela@gmail.com>
2018-01-23 20:04:49 +0100

    Added square root and cube root

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,26 +1,38 @@
 public class Calculator {
 
-    public float add(float a, float b) { return a + b;
+    public float add(float a, float b) {
+        return a + b;
     }
 
-    public float subtract(float a, float b) { return a - b;
+    public float subtract(float a, float b) {
+        return a - b;
     }
 
-    public float multiply(float a, float b) { return a * b;
+    public float multiply(float a, float b) {
+        return a * b;
     }
 
     public float devide(float a, float b) {
-   if (b == 0f) {
+        if (b == 0f) {
             throw new IllegalArgumentException("Division by 0!");
         }
-  return a / b;
+        return a / b;
     }
 
     public int raise(int a, int b) {
         int result = a;
         for (int i = 1; i < b; i++) {
-            result *= a; }
+            result *= a;
+        }
         return result;
+    }
+
+    public double squareRoot(double a) {
+        return Math.sqrt(a);
+    }
+
+    public double cubeRoot(double a) {
+        return Math.cbrt(a);
     }
 }
 
