ca197c13a0543b61fdf4a0d72a2d5ef71fe61f3a
Marcela Friptu <friptu.marcela@gmail.com>
2018-01-23 18:13:54 +0100

    Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Calculator {
+
+    public float plus(float a, float b) { return a + b;
+    }
+
+    public float minus(float a, float b) { return a - b;
+    }
+
+    public float times(float a, float b) { return a * b;
+    }
+
+    public float by(float a, float b) { return a / b;
+    }
 }
+
